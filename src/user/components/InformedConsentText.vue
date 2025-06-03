cca7f7aa2d483a32e80816d986d20bb35c74bbfa
le-gris <solimlegris@gmail.com>
2025-06-03 16:36:30 -0400

    consent form reflect og exp

--- src/user/components/InformedConsentText.vue
+++ src/user/components/InformedConsentText.vue
@@ -1,45 +1,56 @@
 <template>
   <div class="informedconsent">
     <h1 class="is-size-4 has-text-weight-bold">Informed Consent Statement</h1>
-    <hr />
+    <hr>
     <p>
-      If you agree to be in this study, you will be asked to look at pictures of objects or pictures presented on a
-      computer display, watch videos, listen to sounds, or observe lights controlled by the computer and to make
-      judgments about them using a keyboard, mouse, or other input device attached to a computer.
-    </p>
+      You have been invited to take part in a research study named ARC (Abstract Reasoning Corpus). The
+      purpose of this study is to investigate how people learn in dynamic tasks. If you agree to be in this
+      study, you will be asked to look at pictures of objects or pictures presented on a computer display,
+      watch videos, listen to sounds, or observe lights controlled by the computer and to make judgments about
+      them using a keyboard, mouse, or other input device attached to a computer.
     <ul>
       <li>
-        Participation in this study will take about 30 minutes to complete, and you will receive $4.50 for your
-        participation. As an additional incentive to try your best at the task, you may earn a cash bonus of up to
-        $10.00 based on your performance.
-      </li>
-      <li>When you complete the study, a thorough verbal and written explanation of it will be provided.</li>
-      <li>In addition, by agreeing to participate, you understand you must be 18 years or older to participate.</li>
-      <li>
-        There are no known risks associated with your participation in this research beyond those of everyday life.
-        Although you will receive no direct benefits, this research may help the investigator understand how people
-        learn new categories.
+        Participation in this study will take about 30 minutes to complete, and you will receive $10.00 for
+        your participation. As an additional incentive to try your best at the task, you may earn a cash
+        bonus of up to $1.00 based on your performance. Participation in the study is voluntary and you may
+        withdraw at any time. If you withdraw, you will be paid a portion of the full compensation based on
+        the percentage of the study completed.
       </li>
       <li>
-        Confidentiality of your research records will be strictly maintained. We assign code numbers to each participant
-        so that data is never directly linked to individual identity, and we are interested in group results rather than
-        the responses of particular individuals. Any emails will never be shared with anyone nor linked to your data.
-        Data are kept in our laboratory and are only viewed by the investigators. These data files are kept on our
-        computer indefinitely.
+        When you complete the study, a thorough verbal and written explanation of it will be provided.
       </li>
       <li>
-        Taking part in this study is voluntary. Not taking part or withdrawing after the study has begun will result in
-        no loss of services from NYU to which you are otherwise entitled. You have the right to skip or not answer any
-        questions you prefer not to answer.
+        In addition, by agreeing to participate, you understand you must be 18 years or older to
+        participate.
+      </li>
+      <li>
+        You will receive no direct benefits from participating in this study. However, it may make you more
+        aware of how knowledge is discovered in psychology. It may also help researchers better understand
+        how people solve insight problems.
+      </li>
+      <li>
+        Confidentiality of your research records will be strictly maintained. We assign code numbers to each
+        participant so that data is never directly linked to individual identity, and we are interested in
+        group results rather than the responses of particular individuals. Any emails will never be shared
+        with anyone nor linked to your data. Data are kept in our laboratory and are only viewed by the
+        investigators. These data files are kept on our computer indefinitely.
+      </li>
+      <li>
+        Taking part in this study is voluntary. Not taking part or withdrawing after the study has begun
+        will result in no loss of services from NYU to which you are otherwise entitled. You have the right
+        to skip or not answer any questions you prefer not to answer.
       </li>
     </ul>
+    </p>
     <p>
-      If there is anything about the study or your participation that is unclear or that you do not understand, if you
-      have questions or wish to report a research-related problem, you may contact the principal investigator,
-      <a href="http://todd.gureckislab.org">Todd M. Gureckis</a> at 6 Washington Place (409), New York, NY, 10003, (212)
-      998-3794, <a href="mailto:todd.gureckis@nyu.edu">todd.gureckis@nyu.edu</a>. For questions about your rights as a
-      research participant, you may contact the University Committee on Activities Involving Human Subjects, New York
-      University, 665 Broadway, Suite 804, New York, NY 10012, at ask.humansubjects@nyu.edu or (212) 998-4808.
+      If there is anything about the study or your participation that is unclear or that you do not
+      understand, if you have questions or wish to report a research-related problem, you may contact the
+      principal investigator, <a href="http://todd.gureckislab.org">Todd M. Gureckis</a> at 6 Washington Place
+      (409), New York, NY, 10003, (212) 998-3794, <a href="mailto:todd.gureckis@nyu.edu">todd.gureckis@nyu.edu</a>. For
+      questions about your rights as a
+      research participant, you may contact the University Committee on Activities Involving Human Subjects,
+      New York University, 665 Broadway, Suite 804, New York, NY 10012, at ask.humansubjects@nyu.edu or (212)
+      998-4808.
     </p>
   </div>
 </template>
