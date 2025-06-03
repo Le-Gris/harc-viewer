29328b1acbda72d0cd8d6910a0411865bef8aaf3
le-gris <solimlegris@gmail.com>
2025-06-03 16:36:09 -0400

    debrief text reflect og exp

--- src/user/components/DebriefText.vue
+++ src/user/components/DebriefText.vue
@@ -1,8 +1,13 @@
 <template>
   <h1 class="title is-3">What was that about?</h1>
   <div class="debrief">
-    <p>This is where you put the debrief.</p>
-    <p>The purpose of this study was to understand how people learn and make decisions.</p>
+    <p>
+      Thank you for your participation in our study! Your anonymous data makes an important contribution to our
+      understanding of human reasoning.
+    </p>
+    <p>
+      The purpose of this study was to understand how people construct solutions to abstract problems.
+    </p>
   </div>
 </template>
 
